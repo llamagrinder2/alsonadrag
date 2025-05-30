570036e7fe474e86d599946a9a9d068d5a3b1854
llamagrinder2 <attila.raso@outlook.com>
2025-05-30 12:20:27 +0200

    Update game-state.js

--- game-state.js
+++ game-state.js
@@ -1,141 +1,126 @@
 // game-state.js
 
 export const player = {
-    level: 1, // Kezdő szint
+    level: 1,
     currentExp: 0,
-    expToNextLevel: 0, // Ezt is számolni kell
-    maxHp: 0, // Ezt is számolni kell
-    currentHp: 0,
-    baseAttack: 0, // Ezt is számolni kell (Játékos sebzése)
-    attackMultiplier: 1,
-    baseAttackMultiplier: 1, // Boost spell miatt
+    expToNextLevel: 100, // Kezdeti érték, calculateExpToNextLevel felülírja
+    maxHp: 100,
+    currentHp: 100,
+    baseAttack: 0, // Ezt a calculatePlayerStats fogja beállítani
+    attackMultiplier: 1, // Aktív bónuszok miatt változhat
+    baseAttackMultiplier: 1, // Alap attack multiplier (shop itemek növelhetik)
     armor: 0,
-    healStat: 5,
-    potions: { 1: 1, 2: 1, 3: 1 },
     bank: 0,
-    activeSpells: {
-        thirdEye: false,
-        boostSpell: false
+    potions: {
+        1: 1, // Potion LV1: 1 darab
+        2: 0, // Potion LV2: 0 darab
+        3: 0  // Potion LV3: 0 darab
     },
-    // ÚJ: Kockadobás adatok a játékosnak
-    diceCount: 2, // Játékos 2 kockával dob
-    lastRollResults: [], // Itt tároljuk a játékos utolsó dobásainak eredményét
-    currentAction: null // Tárolja a játékos választott akcióját (Attack, Defend, Heal)
+    activeSpells: {
+        thirdEye: false, // Jelenleg inaktív
+        boostSpell: false // Jelenleg inaktív
+    },
+    lastRollResults: [],
+    currentAction: null, // "attack", "defend", "heal"
+    floorLevel: 1 // ÚJ: Kezdeti torony szint
 };
 
 export const mob = {
-    name: "Goblin", // Példa
-    level: 0, // Ezt a player.level alapján fogjuk beállítani
-    maxHp: 0, // Ezt is számolni kell
-    currentHp: 0,
-    baseAttack: 0, // Ezt is számolni kell (MobDamage)
-    armor: 0,
-    xpReward: 0, // Ezt is számolni kell (a PDF alapján finomhangolva)
-    coinReward: 0, // Ezt is számolni kell
-    // Az XP elosztáshoz szükséges, a mob generálásakor kiszámolt alapértékek
-    minMobHpCalculated: 0,
-    maxMobHpCalculated: 0,
-    minMobXpCalculated: 0,
-    maxMobXpCalculated: 0,
-    // ÚJ: Kockadobás adatok a mobnak
-    diceCount: 4, // Mob 4 kockával dob
-    lastRollResults: [], // Itt tároljuk a mob utolsó dobásainak eredményét
-    predictedAction: null, // ÚJ: A mob által választott akció (Attack, Defend, Heal)
-    // ÚJ: Mob akciók esélyei (százalékban)
-    actionChances: {
-        attack: 60, // 60% esély a támadásra
-        defend: 25, // 25% esély a védekezésre
-        heal: 15    // 15% esély a gyógyításra
-    }
+    name: "Goblin",
+    level: 1, // Ez a torony szintjével fog megegyezni
+    maxHp: 50,
+    currentHp: 50,
+    baseAttack: 5, // Ezt a calculateMobStats fogja beállítani
+    xpReward: 0,
+    coinReward: 0,
+    minMobXpCalculated: 0, // A FloorLV alapján számolt min XP
+    maxMobXpCalculated: 0, // A FloorLV alapján számolt max XP
+    minMobGoldCalculated: 0, // A FloorLV alapján számolt min Gold (ÚJ)
+    maxMobGoldCalculated: 0, // A FloorLV alapján számolt max Gold (ÚJ)
+    actionChances: { // Százalékos esélyek a mob akciójára
+        attack: 60,
+        defend: 25,
+        heal: 15
+    },
+    predictedAction: '???',
+    diceCount: 1, // Mob kockák száma
+    lastRollResults: []
 };
 
+// Játék módosítók és konstansok
 export const gameModifiers = {
-    MAX_LEVEL: 50, // Az X érték minden számításhoz
+    MAX_LEVEL: 50, // Maximálisan elérhető szint a játékban (X érték a képletben) [cite: 1]
 
-    // Mob HP
-    MOB_HP_Y: 46.2,
-    MOB_HP_Z_MIN: 16,
-    MOB_HP_Z_MAX: 40,
-
-    // Mob Damage
-    MOB_DAMAGE_Y: 0, // A PDF szerint Y = 0
-    MOB_DAMAGE_Z_MIN: 2,
-    MOB_DAMAGE_Z_MAX: 12,
-
-    // Player Base Damage (ÚJ, a PDF alapján)
-    PLAYER_DAMAGE_Y: 1.5,
-    PLAYER_DAMAGE_Z_MIN: 2,
-    PLAYER_DAMAGE_Z_MAX: 12,
-
-    // Exp for mob (XP Reward)
-    MOB_XP_Y: 42,
-    MOB_XP_Z_MIN: 1,
-    MOB_XP_Z_MAX: 3,
-
-    // Gold Drop Minimum (Y érték pontosítva a legutolsó kérésed szerint)
-    GOLD_DROP_MIN_Y: 49.9899979,
-    GOLD_DROP_MIN_Z: 0.04,
-
-    // Gold Drop Maximum (Y érték pontosítva a legutolsó kérésed szerint)
-    GOLD_DROP_MAX_Y: 49.97999,
-    GOLD_DROP_MAX_Z: 0.08,
-
-    // Exp for player LVL up (Exp To Next Level)
-    PLAYER_LVL_UP_EXP_Y: 49,
-    PLAYER_LVL_UP_EXP_Z: 10,
-
-    // Potion árak (ezekről még nem kaptunk Y és Z paramétereket, fix értékek, ha nem az univerzális képlet szerint számolódnak)
-    POTION_LV1_PRICE: 20,
-    POTION_LV2_PRICE: 40,
-    POTION_LV3_PRICE: 80,
-
-    // Player HP (ezekről még nem kaptunk Y és Z paramétereket, addig is alapértelmezett)
-    // IDEIGLENES, amíg nem kapunk paramétereket
+    // Játékos statisztika módosítók (n = player.level) [cite: 3]
     PLAYER_HP_BASE: 100,
     PLAYER_HP_PER_LEVEL: 10,
+    PLAYER_DAMAGE_Y: 1.5, [cite: 3]
+    PLAYER_DAMAGE_Z_MIN: 2, [cite: 3]
+    PLAYER_DAMAGE_Z_MAX: 12, [cite: 3]
+    PLAYER_LVL_UP_EXP_Y: 42,
+    PLAYER_LVL_UP_EXP_Z: 50,
 
-    // További módosítók (pl. kockadobás maximum értéke)
-    DICE_MAX_VALUE: 6, // *** EZT VÁLTOZTATTAM MEG 10-RŐL 6-RA! ***
+    // Mob statisztika módosítók (n = floorLevel)
+    MOB_HP_Y: 46.2, [cite: 3]
+    MOB_HP_Z_MIN: 16, [cite: 3]
+    MOB_HP_Z_MAX: 40, [cite: 3]
+    MOB_DAMAGE_Y: 47, // Példa érték (ha a PDF nem specifikálta, ez csak egy feltételezés)
+    MOB_DAMAGE_Z_MIN: 1, // Példa érték
+    MOB_DAMAGE_Z_MAX: 3, // Példa érték
 
-    // Védekezés / Gyógyítás mechanika módosítók
+    // XP drop módosítók (n = floorLevel)
+    MOB_XP_Y: 42, [cite: 4]
+    MOB_XP_Z_MIN: 1, [cite: 4]
+    MOB_XP_Z_MAX: 3, [cite: 4]
+
+    // Arany drop módosítók (n = floorLevel) - FRISSÍTVE a legutóbbi pontosítás szerint
+    GOLD_DROP_MIN_Y: 49.97999, [cite: 4]
+    GOLD_DROP_MIN_Z: 0.04, [cite: 4]
+    GOLD_DROP_MAX_Y: 49.9899979, [cite: 4]
+    GOLD_DROP_MAX_Z: 0.08, [cite: 4]
+
+    // Potion árak és gyógyítási módosítók (n = player.level) - FRISSÍTVE
+    POTION_PRICE_SCALAR_LV1: 0.83,
+    POTION_PRICE_SCALAR_LV2: 2,
+    POTION_PRICE_SCALAR_LV3: 5.33,
+    POTION_HEAL_SCALAR_LV1: 0.5, // Példa: átlag gold drop * 0.5 HP gyógyulás
+    POTION_HEAL_SCALAR_LV2: 1.5,
+    POTION_HEAL_SCALAR_LV3: 3.0,
+
+    // Egyéb mechanikák
+    DICE_MAX_VALUE: 6, // Kocka maximális értéke (pl. D6)
     DEFEND_DAMAGE_REDUCTION_PERCENT: 0.5, // 50% sebzéscsökkentés védekezéskor
-    HEAL_POTION_HEAL_AMOUNT: { // Poti gyógyítás mennyisége
-        1: 20,
-        2: 40,
-        3: 80
-    },
-    HEAL_BASE_AMOUNT: 5, // Alap gyógyítás (ha nincs poti)
+    HEAL_BASE_AMOUNT: 5, // Mob gyógyulás alapértéke
+
+    // Spell adatok
+    THIRD_EYE_BOOST: 10, // Kocka dobás bónusz (fix érték)
+    THIRD_EYE_PRICE: 200, // Arany költség (ez is dinamikus lehetne, de most fix)
+    BOOST_SPELL_MULTIPLIER: 0.2, // +20% sebzés
+    BOOST_SPELL_PRICE: 300 // Arany költség (ez is dinamikus lehetne, de most fix)
 };
 
 export const shopItems = {
-    dagger: {
-        name: "Dagger",
-        price: 100,
-        unlocked: false,
-        effect: { attack: 5 } // Példa: +5 ATK
-    },
-    leatherVest: {
-        name: "Leather Vest",
+    // Felszerelés (Példa)
+    bronzeSword: {
+        name: "Bronze Sword",
         price: 150,
-        unlocked: false,
-        effect: { armor: 0.1 } // Példa: +0.1 ARMOR
+        effect: { attack: 0.1 }, // +10% attack multiplier
+        unlocked: false
     },
-    whetstone: {
-        name: "Whetstone",
-        price: 50,
-        unlocked: false,
-        effect: { attack: 2 } // Példa: +2 ATK
+    woodenShield: {
+        name: "Wooden Shield",
+        price: 100,
+        effect: { armor: 5 }, // +5 armor
+        unlocked: false
     },
-    leather2: {
-        name: "Leather Vest 2",
-        price: 200,
-        unlocked: false,
-        effect: { armor: 0.2 } // Példa: +0.2 ARMOR
-    }
+    // Itt nincsenek a potionok, mert azok árát dinamikusan számoljuk
 };
 
+// Shop elemek alapállapotba állítása
 export function resetShopItems() {
     for (const key in shopItems) {
-        shopItems[key].unlocked = false;
+        if (shopItems.hasOwnProperty(key)) {
+            shopItems[key].unlocked = false;
+        }
     }
 }
